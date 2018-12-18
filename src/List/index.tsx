155d706371aab2a6cdb026b69ed4d3c65f1d94ed
Kevin Kam <k040691@icloud.com>
2018-12-18 22:35:04 +0800

    implement react-router and react-loadable

---
+++ src/List/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react"
+
+class List extends React.Component {
+  render() {
+    return <div />
+  }
+}
+
+export default List
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,26 @@
-import * as React from "react";
-import ReactDOM from "react-dom";
+import * as React from "react"
+import ReactDOM from "react-dom"
+import { Switch, Route, Router } from "react-router-dom"
+import loadable from "react-loadable"
+import createBrowserHistory from "history/createBrowserHistory"
+
+const history = createBrowserHistory()
+
+const List = loadable({
+  loader: () => import("./List"),
+  loading: () => <div />
+})
 
 class App extends React.Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route path="/" component={List} />
+        </Switch>
+      </Router>
+    )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"))
