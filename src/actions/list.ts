c34c38c46ebcfa09c3789f8de24d185df9e5ab5f
Kevin Kam <k040691@icloud.com>
2018-12-19 08:17:12 +0800

    added response to redux

--- src/actions/list.ts
+++ src/actions/list.ts
@@ -1,14 +1,37 @@
 import { Dispatch } from "redux"
 import ajax from "util/ajax"
+
+export enum Actions {
+  GET_LIST_REQUEST = "list/GET_LIST_REQUEST",
+  GET_LIST_SUCCESS = "list/GET_LIST_SUCCESS"
+}
+
+interface MovieResult {
+  adult: boolean
+  backdrop_path: string
+  genre_ids: number[]
+  id: number
+  original_language: string
+  original_title: string
+  overview: string
+  popularity: number
+  poster_path: string
+  release_date: string
+  title: string
+  video: boolean
+  vote_average: number
+  vote_count: number
+}
+
+export interface GetListResponse {
+  page: number
+  total_pages: number
+  total_results: number
+  results: MovieResult[]
+}
 export const getList = () => (dispatch: Dispatch) => {
-  ajax
-    .get("/discover/movie", {
-      params: {
-        "primary_release_date.gte": "2014-09-15",
-        "primary_release_date.lte": "2014-10-22"
-      }
-    })
-    .then(response => {
-      console.log(response)
-    })
+  dispatch({ type: Actions.GET_LIST_REQUEST })
+  ajax.get<GetListResponse>("/discover/movie").then(response => {
+    dispatch({ type: Actions.GET_LIST_SUCCESS, payload: response.data })
+  })
 }
--- src/reducers/list.ts
+++ src/reducers/list.ts
@@ -1,6 +1,36 @@
-export const INITIAL_VALUES = []
+import { Actions, GetListResponse } from "../actions/list"
+import { AnyAction } from "redux"
 
-const list = (state = INITIAL_VALUES, actions) => {
+export const INITIAL_VALUES = {
+  movies: [],
+  entities: {},
+  isLoading: true,
+  page: 0,
+  total_pages: 0,
+  total_results: 0
+}
+
+const list = (state = INITIAL_VALUES, actions: AnyAction) => {
+  switch (actions.type) {
+    case Actions.GET_LIST_REQUEST: {
+      return Object.assign({}, state, { isLoading: true })
+    }
+    case Actions.GET_LIST_SUCCESS: {
+      const { results, ...paginateInfo } = actions.payload as GetListResponse
+      return Object.assign({}, state, {
+        ...paginateInfo,
+        isLoading: false,
+        movies: results.map(item => item.id),
+        entities: results.reduce(
+          (r, item) =>
+            Object.assign({}, r, {
+              [item.id]: item
+            }),
+          state.entities
+        )
+      })
+    }
+  }
   return state
 }
 
